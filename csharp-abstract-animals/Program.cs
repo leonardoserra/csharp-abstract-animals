5cae443040dd2652364bbcb307449d2a7dcb828f
leonardoserra <serra.leonardo92@gmail.com>
2023-09-13 16:35:43 +0200

    refactorino esempi tuple e method extension

--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -3,33 +3,42 @@ using csharp_abstract_animals.Interfaces;
 
 namespace csharp_abstract_animals
 {
+
+    //come estendere un method in una classe senza inserirlo dentro la classe stessa
     public static class Extensions
     {
         //estensione di method
         public static void Corri(this Animal animal)
         {
-
             Console.WriteLine("L'animale corre");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
 
             //esempio di generic tuple
             List<(int, double)> ciao = new List<(int, double)>();
             (double, int numero2) asd = (2.0, 3);
             asd.Item1 = 3;
-        }
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+
+
+            //applico un metodo esteso //public static void Corri(this Animal animal)
+            Animal caneEsteso = new Dog();
+            
+            Extensions.Corri(caneEsteso);//modo normale e non immediato
+            caneEsteso.Corri();//syntatic sugar usando il this nella firma
+
             Animal cane = new Dog();
             cane.CosaMangi();
             cane.Dormi();
             cane.Verso();
             Console.WriteLine();
-          
 
-            Extensions.Corri(cane);
-            cane.Corri();
+          
 
             Animal passerotto = new Sparrow();
             passerotto.CosaMangi();
